53be5e19c8e823c6d803d38bee12077e362d02d2
bayuik <bayuindrakusuma05@gmail.com>
2022-06-16 19:12:33 +0700

    update login

--- config/routes.js
+++ config/routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { register, login } = require("../app/controllers");
 
-router.get("/login", login);
+router.post("/login", login);
 router.post("/register", register);
 
 module.exports = router;
