fb8826398ff24ffa9d5f9a01acbed78e7a5a2daf
bayuik <bayuindrakusuma05@gmail.com>
2022-06-17 08:30:15 +0700

    update profile get and put

--- app/index.js
+++ app/index.js
@@ -2,11 +2,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const router = require("../config/routes");
 const cors = require("cors");
-const fs = require("fs");
 const app = express();
-require('dotenv').config();
-// parse application/json
-app.use(bodyParser.json())
+require("dotenv").config();
+app.use(bodyParser.json());
 app.use(express.json());
 app.use(cors());
 app.use(cors());
--- config/routes.js
+++ config/routes.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const router = express.Router();
-const { register, login } = require("../app/controllers");
+const multer = require("multer");
+let storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + ".jpg");
+  },
+});
+const upload = multer({ storage: storage });
+const { register, login, getUser, updateUser } = require("../app/controllers");
 
 router.post("/login", login);
 router.post("/register", register);
+router.put("/profile/:id", upload.single("photo"), updateUser);
+router.get("/profile/:id", getUser);
 
 module.exports = router;
