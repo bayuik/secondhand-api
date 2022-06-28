f2efa13d9cc0c7883ec7df701a7c69a2f163bf87
bayuik <bayuindrakusuma05@gmail.com>
2022-06-28 09:41:44 +0700

    update db data heroku

--- config/database.js
+++ config/database.js
@@ -1,9 +1,10 @@
 const {
-  DB_USERNAME = "postgres",
-  DB_PASSWORD = "admin",
-  DB_HOST = "localhost",
-  DB_NAME = "secondhand",
-  DB_DIALECT = "postgres"
+  DB_USERNAME = "dfbgkzqrsawfom",
+  DB_PASSWORD = "4df4980670897eb9cbbbd6086ce5d668d79ef13f521c1446e0dcddc5a02aa681",
+  DB_HOST = "ec2-3-222-74-92.compute-1.amazonaws.com",
+  DB_NAME = "d5i2g2v5ksu4oj",
+  DB_DIALECT = "postgres",
+  DB_PORT = '5432'
 } = process.env;
 
 module.exports = {
