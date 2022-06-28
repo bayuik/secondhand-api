6593859458ab4733b8efdd685b81f21803c44894
bayuik <bayuindrakusuma05@gmail.com>
2022-06-28 09:46:27 +0700

    update ssl for heroku

--- config/database.js
+++ config/database.js
@@ -4,29 +4,47 @@ const {
   DB_HOST = "ec2-3-222-74-92.compute-1.amazonaws.com",
   DB_NAME = "d5i2g2v5ksu4oj",
   DB_DIALECT = "postgres",
-  DB_PORT = '5432'
+  DB_PORT = "5432",
 } = process.env;
 
 module.exports = {
   development: {
     username: DB_USERNAME,
     password: DB_PASSWORD,
-    database: `${DB_NAME}_development`,
+    database: `${DB_NAME}`,
     host: DB_HOST,
     dialect: DB_DIALECT,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
   test: {
     username: DB_USERNAME,
     password: DB_PASSWORD,
-    database: `${DB_NAME}_test`,
+    database: `${DB_NAME}`,
     host: DB_HOST,
     dialect: DB_DIALECT,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
   production: {
     username: DB_USERNAME,
     password: DB_PASSWORD,
-    database: `${DB_NAME}_production`,
+    database: `${DB_NAME}`,
     host: DB_HOST,
     dialect: DB_DIALECT,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
