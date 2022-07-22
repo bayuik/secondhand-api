50f9cd3914c91d4adeffae5c2ce1fa5bfba34c0d
Bayu Indra Kusuma <bayuindrakusuma05@gmail.com>
2022-07-23 06:39:39 +0700

    update db config

--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
 const {
-  DB_USERNAME = "dfbgkzqrsawfom",
-  DB_PASSWORD = "4df4980670897eb9cbbbd6086ce5d668d79ef13f521c1446e0dcddc5a02aa681",
-  DB_HOST = "ec2-3-222-74-92.compute-1.amazonaws.com",
-  DB_NAME = "d5i2g2v5ksu4oj",
+  DB_USERNAME = "auhirraabthxnr",
+  DB_PASSWORD = "2c490548120d47eb6d89be15aade9962b1cb7385fe5d48b91711bf6611bbdef0",
+  DB_HOST = "ec2-54-86-224-85.compute-1.amazonaws.com",
+  DB_NAME = "dqf1h0krnuc26",
   DB_DIALECT = "postgres",
   DB_PORT = "5432",
 } = process.env;
