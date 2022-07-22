fe8a9e7e188a4ba118c128e4ef77d620f19da7fa
Bayu Indra Kusuma <bayuindrakusuma05@gmail.com>
2022-07-23 06:34:18 +0700

    update db config

--- config/database.js
+++ config/database.js
@@ -1,31 +1,50 @@
 const {
-  DB_USERNAME = "postgres",
-  DB_PASSWORD = "garut123",
-  DB_HOST = "localhost",
-  DB_NAME = "secondhandDB",
-  DB_DIALECT = "postgres"
+  DB_USERNAME = "dfbgkzqrsawfom",
+  DB_PASSWORD = "4df4980670897eb9cbbbd6086ce5d668d79ef13f521c1446e0dcddc5a02aa681",
+  DB_HOST = "ec2-3-222-74-92.compute-1.amazonaws.com",
+  DB_NAME = "d5i2g2v5ksu4oj",
+  DB_DIALECT = "postgres",
+  DB_PORT = "5432",
 } = process.env;
 
 module.exports = {
   development: {
     username: DB_USERNAME,
     password: DB_PASSWORD,
-    database: `${DB_NAME}_development`,
+    database: `${DB_NAME}`,
     host: DB_HOST,
     dialect: DB_DIALECT,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
   test: {
     username: DB_USERNAME,
     password: DB_PASSWORD,
-    database: `${DB_NAME}_test`,
+    database: `${DB_NAME}`,
     host: DB_HOST,
     dialect: DB_DIALECT,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
   production: {
     username: DB_USERNAME,
     password: DB_PASSWORD,
-    database: `${DB_NAME}_production`,
+    database: `${DB_NAME}`,
     host: DB_HOST,
     dialect: DB_DIALECT,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
